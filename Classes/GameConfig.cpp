8f1997e1e76bb167078deca21618a42b791d215b
jiz869 <freeprogrammer@gmail.com>
2014-08-01 16:42:23 -0400

    game config class

--- Classes/GameConfig.cpp
+++ Classes/GameConfig.cpp
@@ -1,5 +1,22 @@
+#include "cocos2d.h"
 #include "GameConfig.h"
 
-GameConfig *GCFG;
+using namespace cocos2d;
 
+GameConfig& GameConfig::GConf()
+{
+    //static variable init only runs once
+    static GameConfig *conf = new GameConfig;
+
+    return *conf;
+}
+
+GameConfig::GameConfig()
+{
+    CCLOG("Init game configuration");
+}
+
+GameConfig::~GameConfig()
+{
+}
 
--- Classes/GameConfig.h
+++ Classes/GameConfig.h
@@ -8,9 +8,18 @@ struct GameConfig : CCObject
 {
     ccColor4F gameWorldBgColor;
 
+public:
+    static GameConfig &GConf();
+    ~GameConfig();
+
+private:
+    GameConfig(); 
+
+    GameConfig(const GameConfig &old); //disallow copy constructor
+    const GameConfig &operator=( const GameConfig &old );
+
 };
 
-extern GameConfig *GCFG;
 
 #endif
 
